8acd9b6ca1947da4128c9090520a17f3010e49ee
Daeki <kimdaeki@gmail.com>
2022-04-22 12:02:42 +0900

    1

--- src/main/java/com/iu/boot3/product/ProductController.java
+++ src/main/java/com/iu/boot3/product/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.iu.boot3.util.Pager;
@@ -21,6 +23,22 @@ public class ProductController {
 	@ModelAttribute("board")
 	public String getBoard() {
 		return "product";
+	}
+	
+	@PostMapping("add")
+	public ModelAndView setAdd(ProductVO productVO, MultipartFile [] files)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		int result = productService.setAdd(productVO, files);
+		mv.setViewName("redirect:./list");
+		return mv;
+	}
+	
+	
+	@GetMapping("add")
+	public ModelAndView setAdd()throws Exception{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("product/add");
+		return mv;
 	}
 	
 	@GetMapping("list")
--- src/main/java/com/iu/boot3/product/ProductService.java
+++ src/main/java/com/iu/boot3/product/ProductService.java
@@ -4,13 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.iu.boot3.util.FileManger;
 import com.iu.boot3.util.Pager;
 
 @Service
 public class ProductService {
 	@Autowired
 	private ProductMapper productMapper;
+	@Autowired
+	private FileManger fileManger;
+	
+	public int setAdd(ProductVO productVO, MultipartFile [] files)throws Exception{
+		int result = productMapper.setAdd(productVO);
+		
+		for(MultipartFile f : files) {
+			if(f.isEmpty()) {
+				continue;
+			}
+			
+			String fileName= fileManger.fileSave(f, "resources/upload/product/");
+			ProductFilesVO productFilesVO = new ProductFilesVO();
+			productFilesVO.setProductNum(productVO.getProductNum());
+			productFilesVO.setFileName(fileName);
+			productFilesVO.setOriName(f.getOriginalFilename());
+			result = productMapper.setFileAdd(productFilesVO);
+		}
+		return result;
+	}
 	
 	public List<ProductVO> getList(Pager pager)throws Exception{
 		pager.makeRow();
