7ac38a650aa745456d7b07d821f448240230b647
Daeki <kimdaeki@gmail.com>
2022-04-26 17:35:26 +0900

    1

--- src/main/java/com/iu/boot3/board/BoardService.java
+++ src/main/java/com/iu/boot3/board/BoardService.java
@@ -1,21 +1,38 @@
 package com.iu.boot3.board;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.iu.boot3.util.FileManger;
 import com.iu.boot3.util.Pager;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class BoardService {
 	@Autowired
 	private BoardMapper boardMapper;
 	
 	@Autowired
 	private FileManger fileManger;
+	
+	public boolean setSummerFileDelete(String fileName)throws Exception{
+		fileName = fileName.substring(fileName.lastIndexOf("/")+1);
+		System.out.println(fileName);
+		
+		return fileManger.fileDelete(fileName, "/resources/upload/board/");
+	}
+	
+	public String setSummerFileUpload(MultipartFile files)throws Exception{
+		// file HDD에 저장하고 저장된 파일명을 return
+		String fileName = fileManger.fileSave(files, "resources/upload/board");
+		fileName = "/resources/upload/board/"+fileName;
+		return fileName;
+	}
 	
 	public BoardFilesVO getFileDetail(BoardFilesVO boardFilesVO)throws Exception{
 		return boardMapper.getFileDetail(boardFilesVO);
@@ -47,22 +64,31 @@ public class BoardService {
 		int result = boardMapper.setAdd(boardVO);
 		System.out.println("Insert 후 : "+boardVO.getNum());
 		
+		if(files != null && result >0) {
 		
-		for(MultipartFile mf : files) {
-			
-			if(mf.isEmpty()) {
-				continue;
+			for(MultipartFile mf : files) {
+				
+				if(mf.isEmpty()) {
+					continue;
+				}
+
+				
+				//1. File을 HDD에 저장
+				 String fileName = fileManger.fileSave(mf, "resources/upload/board/");
+				 System.out.println(fileName);
+				//2. 저장된 정보를 DB에 저장
+				 BoardFilesVO boardFilesVO = new BoardFilesVO();
+				 boardFilesVO.setNum(boardVO.getNum());
+				 boardFilesVO.setFileName(fileName);
+				 boardFilesVO.setOriName(mf.getOriginalFilename());
+				 result = boardMapper.setFileAdd(boardFilesVO);
+				 
+				 if(result <1) {
+					 //board table insert한 것도 rollback
+					 throw new SQLException();
+				 }
+				 
 			}
-			
-			//1. File을 HDD에 저장
-			 String fileName = fileManger.fileSave(mf, "resources/upload/board/");
-			 System.out.println(fileName);
-			//2. 저장된 정보를 DB에 저장
-			 BoardFilesVO boardFilesVO = new BoardFilesVO();
-			 boardFilesVO.setNum(boardVO.getNum());
-			 boardFilesVO.setFileName(fileName);
-			 boardFilesVO.setOriName(mf.getOriginalFilename());
-			 boardMapper.setFileAdd(boardFilesVO);
 		}
 		
 		return result;//
@@ -71,7 +97,8 @@ public class BoardService {
 	public List<BoardVO> getList(Pager pager)throws Exception{
 		pager.makeRow();
 		pager.makeNum(boardMapper.getTotalCount(pager));
-		
+		System.out.println("start : "+pager.getStartNum());
+		System.out.println("perPage : "+pager.getPerPage());
 		return boardMapper.getList(pager);
 	}
 
--- src/main/webapp/WEB-INF/views/board/add.jsp
+++ src/main/webapp/WEB-INF/views/board/add.jsp
@@ -65,7 +65,43 @@
 
 <script type="text/javascript">
 	$("#contents").summernote({
-		height:400
+		height:400,
+		placeholder:'내용을 입력하세요',
+		callbacks: {
+			onImageUpload:function(files){
+				//files upload한 이미지 파일객체
+				let formData = new FormData();
+				formData.append("files", files[0]);
+				
+				// /board/summerFileUpload
+				$.ajax({
+					type:"POST",
+					url:"./summerFileUpload",
+					data:formData,
+					contentType:false,
+					processData:false,
+					success:function(data){
+						$("#contents").summernote('editor.insertImage', data.trim());
+					}
+					
+				});
+				
+			},//onImageUpload 끝
+			onMediaDelete:function(files){
+				let fileName = $(files[0]).attr("src");
+				console.log(fileName);
+				$.ajax({
+					type:"GET",
+					url: "./summerFileDelete",
+					data:{
+						fileName:fileName
+					},
+					success:function(data){
+						console.log(data);
+					}
+				});
+			}//onMediaDelete 끝
+		}
 	});
 
 
